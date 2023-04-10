8980971dc1623aa8f614b9e37350eaa1c12a13ed
Nico Kokonas <nico@nicomee.com>
2023-04-09 19:39:00 -0600

    feat(refctr)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,40 +10,40 @@ namespace NetgearHammerv2 {
 
         static async Task Main (string[] args) {
             string rootfilename = Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location);
-            Console.WriteLine($"Current Working Directory: {rootfilename}");
+            Console.WriteLine ($"Current Working Directory: {rootfilename}");
 
             string filename = rootfilename + "\\output.csv";
-            Console.WriteLine($"Output will be saved at: {filename}.");
+            Console.WriteLine ($"Output will be saved at: {filename}.");
 
             Console.WriteLine ("Enter desired number of attempts (ex. 200):" + Environment.NewLine);
-            var attemptsCount = Console.ReadLine ();
+            string attemptsCount = Console.ReadLine ();
 
-            Console.WriteLine ("Running through " + attemptsCount.ToString () + " iterations, now select your desired prodcut...." + Environment.NewLine + " (1) ReadyNAS RNDP6000" + " (2) ReadyNAS 516" + " (3) ReadyNAS 716X" + " (4) R8000 Router" + " (5) R8500 Router" + " (6) A6210 WiFi USB Adapter" + " (7) ProSAFE M7300-24XF Switch" + " (8) ReadyNAS 526X" + " (9) ReadyNAS 528X" + " (10) R9000 Router" + " (11) ProSAFE XS728T Switch" + " (12) ProSAFE XS748T Switch" + " (13) ReadyNAS 3312" + " (14) ReadyNAS RN626X00" + " (15) ReadyNAS RR4312X" + " (16) ReadyNAS RR4360X" + Environment.NewLine);
-            var productChoice = Console.ReadLine ();
+            Console.WriteLine ("Running through " + attemptsCount + " iterations, now select your desired prodcut...." + Environment.NewLine + " (1) ReadyNAS RNDP6000" + " (2) ReadyNAS 516" + " (3) ReadyNAS 716X" + " (4) R8000 Router" + " (5) R8500 Router" + " (6) A6210 WiFi USB Adapter" + " (7) ProSAFE M7300-24XF Switch" + " (8) ReadyNAS 526X" + " (9) ReadyNAS 528X" + " (10) R9000 Router" + " (11) ProSAFE XS728T Switch" + " (12) ProSAFE XS748T Switch" + " (13) ReadyNAS 3312" + " (14) ReadyNAS RN626X00" + " (15) ReadyNAS RR4312X" + " (16) ReadyNAS RR4360X" + Environment.NewLine);
+            string productChoice = Console.ReadLine ();
 
             Console.WriteLine ("Enter netgear email address: ");
-            var emailAddress = Console.ReadLine ().Trim ();
+            string emailAddress = Console.ReadLine ().Trim ();
 
             Console.WriteLine ("Enter password: ");
-            var password = Console.ReadLine ().Trim ();
+            string password = Console.ReadLine ().Trim ();
 
             int invalidCounter = 0;
 
             // Use puppeteer for automation
             await new BrowserFetcher ().DownloadAsync (BrowserFetcher.DefaultRevision);
-            var browser = await Puppeteer.LaunchAsync (new LaunchOptions {
-                Headless = false
+            Browser browser = await Puppeteer.LaunchAsync (new LaunchOptions {
+                Headless = false,
             });
 
-            var page = await browser.NewPageAsync ();
+            Page page = await browser.NewPageAsync ();
             // accept any dialog prompts
             page.Dialog += async (sender, e) =>
             {
-                Console.WriteLine("Intercepted dialog!");
-                var msg = e.Dialog.Message;
-                Console.WriteLine(msg);
+                Console.WriteLine ("Intercepted dialog!");
+                string msg = e.Dialog.Message;
+                Console.WriteLine (msg);
 
-                await e.Dialog.Dismiss();
+                await e.Dialog.Dismiss ();
             };
 
             await page.SetUserAgentAsync ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
@@ -63,38 +61,38 @@ namespace NetgearHammerv2 {
                 string tempSerial = GenerateSerial (productChoice);
 
                 // Check serial validity
-                var result = await checkSerial (tempSerial, page);
+                string result = await checkSerial (tempSerial, page);
 
-                if (result.Contains("None")) {
+                if (result.Contains ("None")) {
                     invalidCounter++;
                 } else {
                     Console.WriteLine ($"[+] Valid Serial: {tempSerial}");
-                    var productData = await extractData(tempSerial, page);
+                    string productData = await extractData (tempSerial, page);
 
-                    Console.WriteLine($"Extracted product details: {productData}");
+                    Console.WriteLine ($"Extracted product details: {productData}");
                     validserials.AppendLine (tempSerial + "," + productData);
                 }
 
             }
 
             // write to csv
-            File.AppendAllText(filename, validserials.ToString());
+            File.AppendAllText (filename, validserials.ToString ());
 
-            Console.WriteLine ("We found " + invalidCounter.ToString () + " invalid serials out of the total attempt count of " + attemptsCount.ToString ());
+            Console.WriteLine ("We found " + invalidCounter + " invalid serials out of the total attempt count of " + attemptsCount);
 
         }
 
-        static async Task<string> extractData(string serialNumber, Page page) {
-            await page.GoToAsync("https://www.netgear.com/mynetgear/portal/myProducts.aspx");
-            await page.WaitForSelectorAsync(".product-table");
+        static async Task<string> extractData (string serialNumber, Page page) {
+            await page.GoToAsync ("https://www.netgear.com/mynetgear/portal/myProducts.aspx");
+            await page.WaitForSelectorAsync (".product-table");
 
-            await page.EvaluateExpressionAsync<dynamic>($"javascript:ShowInfo('{serialNumber}')");
-            await page.WaitForTimeoutAsync(5000);
+            await page.EvaluateExpressionAsync<dynamic> ($"javascript:ShowInfo('{serialNumber}')");
+            await page.WaitForTimeoutAsync (5000);
 
             string rootfilename = Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location);
-            await page.ScreenshotAsync($"{rootfilename}\\{serialNumber}.png");
+            await page.ScreenshotAsync ($"{rootfilename}\\{serialNumber}.png");
 
-            var productData = await page.EvaluateExpressionAsync<dynamic>("document.querySelector('#formPop').innerText");
+            dynamic productData = await page.EvaluateExpressionAsync<dynamic> ("document.querySelector('#formPop').innerText");
             return productData;
         }
 
@@ -102,62 +100,62 @@ namespace NetgearHammerv2 {
             string productInfo = "None";
 
             try {
-            await page.GoToAsync ("https://my.netgear.com/register.aspx");
-            await page.WaitForSelectorAsync ("input#MainContent_serialNumberInput");
+                await page.GoToAsync ("https://my.netgear.com/register.aspx");
+                await page.WaitForSelectorAsync ("input#MainContent_serialNumberInput");
 
-            string[] days = {
-                "08",
-                "09",
-                "10",
-                "11",
-                "12",
-                "13",
-                "14",
-                "15",
-                "16",
-                "17",
-                "18",
-                "19",
-                "20",
-                "21",
-                "22",
-                "23",
-                "24",
-                "25",
-                "26",
-                "27",
-                "28",
-                "29",
-                "30",
-                "31",
-            };
-            string[] months = {
-                "03",
-            };
-            string[] years = {
-                "2021",
-            };
+                string[] days = {
+                    "08",
+                    "09",
+                    "10",
+                    "11",
+                    "12",
+                    "13",
+                    "14",
+                    "15",
+                    "16",
+                    "17",
+                    "18",
+                    "19",
+                    "20",
+                    "21",
+                    "22",
+                    "23",
+                    "24",
+                    "25",
+                    "26",
+                    "27",
+                    "28",
+                    "29",
+                    "30",
+                    "31",
+                };
+                string[] months = {
+                    "03",
+                };
+                string[] years = {
+                    "2021",
+                };
 
-            Random rand = new Random();
-            int dayIndex = rand.Next(days.Length);
-            int monthIndex = rand.Next(months.Length);
-            int yearIndex = rand.Next(years.Length);
+                Random rand = new Random ();
+                int dayIndex = rand.Next (days.Length);
+                int monthIndex = rand.Next (months.Length);
+                int yearIndex = rand.Next (years.Length);
 
-            await page.TypeAsync ("input#MainContent_serialNumberInput", serialNumber);
-            await page.SelectAsync ("#MainContent_ddlMonth", $"{months[monthIndex]}");
-            await page.SelectAsync ("#MainContent_ddlDay", $"{days[dayIndex]}");
-            await page.SelectAsync ("#MainContent_ddlYear", $"{years[yearIndex]}");
+                await page.TypeAsync ("input#MainContent_serialNumberInput", serialNumber);
+                await page.SelectAsync ("#MainContent_ddlMonth", $"{months[monthIndex]}");
+                await page.SelectAsync ("#MainContent_ddlDay", $"{days[dayIndex]}");
+                await page.SelectAsync ("#MainContent_ddlYear", $"{years[yearIndex]}");
 
-            await page.ClickAsync ("#MainContent_btnSubmit");
-            await page.WaitForTimeoutAsync(3500);
+                await page.ClickAsync ("#MainContent_btnSubmit");
+                await page.WaitForTimeoutAsync (3500);
 
-            var result = await page.EvaluateExpressionAsync<dynamic>("((document.querySelector('#MainContent_errorStatus,.activeServerError') || {}).innerText || '').trim();");
+                dynamic result = await page.EvaluateExpressionAsync<dynamic> ("((document.querySelector('#MainContent_errorStatus,.activeServerError') || {}).innerText || '').trim();");
 
-            if (result.IndexOf ("not valid") > 0 || result.IndexOf("invalid") > 0 || result.indexOf("already registered") > 0) {
-                Console.WriteLine ($"[-] Invalid Serial: {serialNumber}");
-            }
+                if (result.IndexOf ("not valid") > 0 || result.IndexOf ("invalid") > 0 || result.indexOf ("already registered") > 0) {
+                    Console.WriteLine ($"[-] Invalid Serial: {serialNumber}");
+                }
             } catch (Exception e) {
-                Console.WriteLine(e.Message);
+                Console.WriteLine (e.Message);
                 productInfo = "VALID";
             }
 
@@ -189,15 +187,15 @@ namespace NetgearHammerv2 {
                 case "11":
                     return "49E" + RandomNum (3) + "5T" + RandomNum (5);
                 case "12":
-                    return "4M0" + RandomNum (3) + "5R" + RandomNum(5);
+                    return "4M0" + RandomNum (3) + "5R" + RandomNum (5);
                 case "13":
-                    return "4US" + RandomNum(3) + "ER" + RandomNum(5);
+                    return "4US" + RandomNum (3) + "ER" + RandomNum (5);
                 case "14":
-                    return "4MD" + RandomNum(3) + "EC" + RandomNum(5);
+                    return "4MD" + RandomNum (3) + "EC" + RandomNum (5);
                 case "15":
-                    return "4UT" + RandomNum(3) + "ES" + RandomNum(5);
+                    return "4UT" + RandomNum (3) + "ES" + RandomNum (5);
                 case "16":
-                    return "4VW" + RandomNum(3) + "E8" + RandomNum(5);
+                    return "4VW" + RandomNum (3) + "E8" + RandomNum (5);
                 default:
                     return "Invalid type specified.";
 
